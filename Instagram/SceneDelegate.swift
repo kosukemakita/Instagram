e361b85390a5923f24b382f5aebea36c3ec1831d
kosukemakita <makitakosuke0114@gmail.com>
2020-11-01 16:27:56 +0900

    謎にビルドが失敗 SVProgressHUD

--- Instagram/SceneDelegate.swift
+++ Instagram/SceneDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+//import SVProgressHUD
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -18,6 +20,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        // SVProgressHUDをXcode11で実行するための環境調整コード
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.window = self.window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
